02e1152707b3a04c92685c193c00fa884fb05c28
Prateek Tripathi <73139653+er-prateek-tripathi@users.noreply.github.com>
2022-04-16 08:17:12 +0530

    Add files via upload

---
+++ Frontend/src/components/control-bar/ControlBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ActionButton from "../control-options/ActionButton";
+import ViewSelect from "../control-options/ViewSelect";
+
+const ControlBar = ({ handleSearchValue }) => {
+  return (
+    <div className="control-bar">
+      <ViewSelect />
+      <div>
+        <input
+          type="text"
+          placeholder="Search Customer Id"
+          className="search-bar"
+          onChange={(e) => {
+            handleSearchValue(e.target.value);
+            console.log(handleSearchValue);
+          }}
+        />
+      </div>
+      
+      <ActionButton />
+    </div>
+  );
+};
+
+export default ControlBar;
